8b7a5404255c0f4c09f69599a192976e6959284b
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-01-28 18:48:28 +0530

    'password-generator-2801251848'

---
+++ unique-password-generator.py
@@ -0,0 +1,23 @@
+import secrets
+import string
+
+
+def generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    password = ''
+    # Generate password
+    for _ in range(length):
+        password += secrets.choice(all_characters)
+        
+    return password
+    
+password_length=int(input('Enter new password length: '))
+new_password = generate_password(password_length)
+print (new_password)
